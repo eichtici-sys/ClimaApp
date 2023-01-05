fbfb28886fc653c26f95a725933f8eda6a3c3218
eichtici-sys <henryjesus.htc@gmail.com>
2023-01-04 19:29:40 -0500

    fixed ssl

--- src/context/ClimaProvide.jsx
+++ src/context/ClimaProvide.jsx
@@ -25,7 +25,7 @@ const ClimaProvider = ({ children }) => {
     try {
       const { ciudad, pais } = datos;
       const appId = import.meta.env.VITE_API_KEY;
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
+      const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
 
       const { data } = await axios(url);
 
